6f99bcc0ad2e4041a4a3ec4f4c5ec7db6b6b74bb
Ben Redmond <benjamin.j.redmond@vanderbilt.edu>
2021-12-07 13:49:42 -0500

    update schema

--- src/realm/schema.ts
+++ src/realm/schema.ts
@@ -1,52 +1,57 @@
 const FunctionSchema = {
-    "title": "function_registry",
-    "properties": {
-        "_id": {
-            "bsonType": "objectId"
-        },
-        "downloads": {
-            "bsonType": "array",
-            "items": {
-                "bsonType": "string"
-            }
-        },
-        "owner_id": {
-            "bsonType": "objectId"
-        },
-        "raw": {
-            "bsonType": "string",
-            "properties": {}
-        },
-        "tags": {
-            "bsonType": "array",
-            "items": {
-                "type": "string"
-            }
-        },
-        "name": {
-            "bsonType": "string"
-        }
-    }
-}
+  title: "function_registry",
+  properties: {
+    _id: {
+      bsonType: "objectId",
+    },
+    downloads: {
+      bsonType: "array",
+      items: {
+        bsonType: "string",
+      },
+    },
+    owner_id: {
+      bsonType: "objectId",
+    },
+    raw: {
+      bsonType: "string",
+      properties: {},
+    },
+    tags: {
+      bsonType: "array",
+      items: {
+        type: "string",
+      },
+    },
+    name: {
+      bsonType: "string",
+    },
+    dependencies: {
+      bsonType: "array",
+      items: {
+        bsonType: "string",
+      },
+    },
+  },
+};
 
 const UserSchema = {
-    "title": "user",
-    "properties": {
-        "_id": {
-            "bsonType": "objectId"
-        },
-        "authored_functions": {
-            "bsonType": "array",
-            "items": {
-                "bsonType": "objectId"
-            }
-        },
-        "email": {
-            "bsonType": "string"
-        }
-    }
-}
+  title: "user",
+  properties: {
+    _id: {
+      bsonType: "objectId",
+    },
+    authored_functions: {
+      bsonType: "array",
+      items: {
+        bsonType: "objectId",
+      },
+    },
+    email: {
+      bsonType: "string",
+    },
+  },
+};
 
-
-exports.UserSchema = UserSchema
-exports.FunctionSchema = FunctionSchema
+exports.UserSchema = UserSchema;
+exports.FunctionSchema = FunctionSchema;
